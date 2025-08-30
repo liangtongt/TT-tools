a64388981c1d31925468c2fae27378768d75477e
梁通通 <394115207@qq.com>
2025-08-30 23:00:41 +0800

    修改说明

--- tt_img_enc_node.py
+++ tt_img_enc_node.py
@@ -22,7 +22,7 @@ class TTImgEncNode:
                 "quality": ("INT", {"default": 95, "min": 1, "max": 100}),
             },
             "optional": {
-                "usage_notes": ("STRING", {"default": "  利用图片的像素信息保存视频或图片，配合解码本地解码，达到破限的目的/nB站：我是小斯呀", "multiline": True}),
+                "usage_notes": ("STRING", {"default": "  利用图片的像素信息保存视频或图片\n配合解码本地解码，达到破限的目的\nB站：我是小斯呀\n教程：https://b23.tv/RbvaMeW", "multiline": True}),
             }
         }
     
